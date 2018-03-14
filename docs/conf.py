542461939b876a000e09cbe92fba6ca147c50086
Max Fischer <maxfischer2781@gmail.com>
2018-03-14 15:17:22 +0100

    docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -87,6 +87,9 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = False
 
 # -- Options for HTML output ----------------------------------------------
 
