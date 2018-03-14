e5f577dc65c8dbab696cdd366645b65ef73281ab
Max Fischer <maxfischer2781@gmail.com>
2018-03-14 11:26:29 +0100

    about file

---
+++ xrootdlib/__about__.py
@@ -0,0 +1,17 @@
+"""
+============================================================
+``xrootdlib`` - Tools for working with the XRootD middleware
+============================================================
+
+The ``xrootdlib`` offers building blocks and basic tools to work with the XRootD data access middleware.
+It is meant to facilitate auxiliary work, such as monitoring, accounting and orchestration.
+"""
+
+__title__ = 'xrootdlib'
+__summary__ = 'library for working with the XRootD middleware'
+__url__ = 'https://github.com/maxfischer2781/xrootdlib'
+
+__version__ = '0.1.0pre'
+__author__ = 'Max Fischer'
+__email__ = 'maxfischer2781@gmail.com'
+__copyright__ = '2018 %s' % __author__
